5bbd8d7833d7dbefdb28e571e79503ab2641d281
codewithabhi8 <codewithabhi8@gmail.com>
2025-12-04 07:08:30 +0530

    Few patterns practice TUF

---
+++ TUF/Patterns/PythogorusNumbers.java
@@ -0,0 +1,12 @@
+package TUF.Patterns;
+
+public class PythogorusNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ TUF/Patterns/ReverseTriangleNumbers.java
@@ -0,0 +1,12 @@
+package TUF.Patterns;
+
+public class ReverseTriangleNumbers {
+    public static void main(String[] args) {
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
