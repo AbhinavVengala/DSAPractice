b24210bf8846c52334133e8a02973327cba435db
codewithabhi8 <codewithabhi8@gmail.com>
2026-02-17 12:38:22 +0530

    2 Pyramid patterns

---
+++ TUF/Patterns/StarsReversePyramid.java
@@ -0,0 +1,19 @@
+package TUF.Patterns;
+
+public class StarsReversePyramid {
+    public static void main(String[] args) {
+        int N = 5;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (N - i) - 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j < N; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
