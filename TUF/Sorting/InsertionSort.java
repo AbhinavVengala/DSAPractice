2a29513b3349f35fde3d30934a32b4618618b19f
codewithabhi8 <codewithabhi8@gmail.com>
2026-09-14 21:18:20 +0530

    Sorting part 1

---
+++ TUF/Sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+package TUF.Sorting;
+
+public class InsertionSort {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+        for (int i = 0; i<=arr.length - 1; i++) {
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+}
